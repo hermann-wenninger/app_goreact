af372671e53d4ee6ce9092716de988f2a0f57156
hermann wenninger <info@wennhmann.de>
2024-01-15 15:42:09 +0100

    goroutine

--- lectionTwo/main.go
+++ lectionTwo/main.go
@@ -3,11 +3,13 @@ import(
 	"wennhmann.de/lectionTwo/fill"
 	 xyz "wennhmann.de/lectionTwo/init"
 	"fmt"
+	"time"
 )
 
 func main(){
 	fill.Fill()
 	fmt.Println(xyz.Init)
-	 go fill.PrintNum_0()
-	 fill.PrintNum_1()
+	go fill.PrintNum_0()
+	go fill.PrintNum_1()
+	time.Sleep(1 * time.Minute) 
 }
