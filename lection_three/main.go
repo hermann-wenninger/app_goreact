5df176fcdb5f508316a2af1c424a39b89971755b
hermann wenninger <info@wennhmann.de>
2024-01-18 12:53:08 +0100

    .gitignore vscode

--- lection_three/main.go
+++ lection_three/main.go
@@ -7,10 +7,10 @@ import (
 )
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 r.ParseForm() // parse arguments, you have to call this by yourself
-fmt.Println(r.Form) // print form information in server side
-fmt.Println("path", r.URL.Path)
-fmt.Println("scheme", r.URL.Scheme)
-fmt.Println(r.Form["url_long"])
+fmt.Println("formc",r.Form) // print form information in server side
+fmt.Println("pathc", r.URL.Path)
+fmt.Println("schemec", r.URL.Scheme)
+fmt.Println("the key of form rrr",r.Form["url_long"])
 for k, v := range r.Form {
 fmt.Println("key:", k)
 fmt.Println("val:", strings.Join(v, ""))
